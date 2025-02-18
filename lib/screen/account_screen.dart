0c45324ab194ce5d5de3efeaecacde616f7faeae
wildanuwil <wildanfajriyann@gmail.com>
2025-02-18 11:51:59 +0700

    add user info

--- lib/screen/account_screen.dart
+++ lib/screen/account_screen.dart
@@ -5,8 +5,80 @@ class AccountScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Akun Anda...'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Akun',
+          style: Theme.of(context)
+              .textTheme
+              .titleSmall
+              ?.copyWith(fontWeight: FontWeight.w600, color: Colors.white),
+        ),
+        backgroundColor: Colors.teal.shade700,
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 12, vertical: 20),
+            padding: EdgeInsets.all(14),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(2)),
+                color: Colors.teal.shade100),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxWidth: 50,
+                    minWidth: 50,
+                    maxHeight: 50,
+                    minHeight: 50,
+                  ),
+                  child: CircleAvatar(
+                    child: ClipOval(
+                      child: Image.network(
+                        'https://static.vecteezy.com/system/resources/thumbnails/005/544/718/small_2x/profile-icon-design-free-vector.jpg',
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Bob Bobby',
+                      style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                            fontWeight: FontWeight.w600,
+                          ),
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      'Aktif Berlangganan',
+                      style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                            fontWeight: FontWeight.w300,
+                          ),
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      '01 Jan 2021 - 01 Jan 2022',
+                      style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                            fontWeight: FontWeight.w600,
+                          ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
